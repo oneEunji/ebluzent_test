90c6d302247d4dbe52ea68ad4c842f9a3c20cc3b
eunjikim <ebluzent_member1@ebluzentgroup.com>
2022-03-08 09:20:16 +0900

    2022.03.07

--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -62,7 +62,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    @WithMockUser(roles="USER")
+//    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
