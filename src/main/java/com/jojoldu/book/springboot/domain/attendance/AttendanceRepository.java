6b220d315dfb4ca1b565b86a39e3f2a8f43e3e94
eunjikim <ebluzent_member1@ebluzentgroup.com>
2022-03-08 14:22:09 +0900

    2022.03.07

--- src/main/java/com/jojoldu/book/springboot/domain/attendance/AttendanceRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/attendance/AttendanceRepository.java
@@ -2,9 +2,11 @@ package com.jojoldu.book.springboot.domain.attendance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 
     @Query("SELECT a FROM Attendance a ORDER BY a.attendanceId DESC")
--- src/main/java/com/jojoldu/book/springboot/domain/company/CompanyRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/company/CompanyRepository.java
@@ -3,9 +3,11 @@ package com.jojoldu.book.springboot.domain.company;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     @Query("SELECT c FROM Company c ORDER BY c.companyId DESC")
